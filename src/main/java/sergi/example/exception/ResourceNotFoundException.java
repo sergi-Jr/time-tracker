27387d15bdc37fef6f9ad6371d698dbf01d6677b
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-07-22 20:42:36 +0400

    add resource not found ex

---
+++ src/main/java/sergi/example/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package sergi.example.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
