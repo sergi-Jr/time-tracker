7f0bad1d1b5c9927719dfc1839cd00e7dee1e7d1
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-07-22 16:40:10 +0400

    add initial dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
+	application
+	checkstyle
+	jacoco
+	id("com.github.johnrengelman.shadow") version "8.1.1"
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+	id("io.freefair.lombok") version "8.6"
 }
 
 group = "sergi.example"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+application {
+	mainClass = "sergi.example.TimeTrackerApplication"
 }
 
 repositories {
@@ -18,11 +23,48 @@ repositories {
 }
 
 dependencies {
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	implementation("net.datafaker:datafaker:2.2.2")
+	implementation("org.instancio:instancio-junit:4.8.0")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.postgresql:postgresql:42.1.4")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	annotationProcessor("org.projectlombok:lombok:1.18.32")
+
+	compileOnly("org.projectlombok:lombok:1.18.32")
+	runtimeOnly("com.h2database:h2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(platform("org.junit:junit-bom:5.10.2"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
-tasks.withType<Test> {
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport)
 	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStandardStreams = true
+	}
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+	}
 }
