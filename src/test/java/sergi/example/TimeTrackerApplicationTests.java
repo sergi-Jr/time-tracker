f062ff0e66eba5ffcce7fbc11b41bda2c8534b9a
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-07-22 16:29:19 +0400

    init commit

---
+++ src/test/java/sergi/example/TimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package sergi.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
