dafb0135ace72454593fb1d609cb1136f8c0cbd3
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-15 20:50:23 +0400

    [skip ci] add repository annotation

--- src/main/java/sergi/example/user/dal/UserRepository.java
+++ src/main/java/sergi/example/user/dal/UserRepository.java
@@ -1,7 +1,9 @@
 package sergi.example.user.dal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sergi.example.user.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
