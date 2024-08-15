72be2b10634038b8a636485e3031effc55369c77
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-15 20:51:41 +0400

    [skip ci] add tasks relation

--- src/main/java/sergi/example/user/User.java
+++ src/main/java/sergi/example/user/User.java
@@ -1,11 +1,14 @@
 package sergi.example.user;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -14,8 +17,11 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import sergi.example.task.Task;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -33,6 +39,19 @@ public class User {
     private String email;
 
     private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.MERGE)
+    private List<Task> tasks = new ArrayList<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        task.setUser(this);
+    }
+
+    public void removeTask(Task task) {
+        tasks.removeIf(t -> t.getId() == task.getId());
+        task.setUser(null);
+    }
 
     @CreatedDate
     private LocalDate createdAt;
