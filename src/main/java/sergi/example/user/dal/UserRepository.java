d838c22576809acd54e3b4e4da0c1ea69b8e5edb
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-15 22:11:06 +0400

    [skip ci] added find by email search

--- src/main/java/sergi/example/user/dal/UserRepository.java
+++ src/main/java/sergi/example/user/dal/UserRepository.java
@@ -1,9 +1,14 @@
 package sergi.example.user.dal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import sergi.example.user.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
 }
