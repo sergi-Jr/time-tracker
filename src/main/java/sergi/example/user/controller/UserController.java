8cdfd4f08b776db48a37d46a79d6baf89ae79cbf
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-15 22:10:40 +0400

    [skip ci] minor fixes

--- src/main/java/sergi/example/user/controller/UserController.java
+++ src/main/java/sergi/example/user/controller/UserController.java
@@ -1,13 +1,16 @@
 package sergi.example.user.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import sergi.example.user.dal.UserService;
 import sergi.example.user.dto.UserCreateDTO;
@@ -15,21 +18,19 @@ import sergi.example.user.dto.UserDTO;
 import sergi.example.user.dto.UserUpdateDTO;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService service) {
-        userService = service;
-    }
-
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDTO create(@Valid @RequestBody UserCreateDTO data) {
         return userService.create(data);
     }
 
-    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(path = "/{id}")
     public UserDTO update(@Valid @RequestBody UserUpdateDTO data, @PathVariable Long id) {
         return userService.update(id, data);
     }
