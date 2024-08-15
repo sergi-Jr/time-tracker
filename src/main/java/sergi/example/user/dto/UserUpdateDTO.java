1cd84d393c0f6512d37d3e11b1818f6692434a9a
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-15 22:11:28 +0400

    [skip ci] add dto for updating user

--- src/main/java/sergi/example/user/dto/UserUpdateDTO.java
+++ src/main/java/sergi/example/user/dto/UserUpdateDTO.java
@@ -1,4 +1,15 @@
 package sergi.example.user.dto;
 
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Getter
+@Setter
 public class UserUpdateDTO {
+    private JsonNullable<String> email;
+
+    @Size(min = 3, max = 100)
+    private JsonNullable<String> name;
 }
