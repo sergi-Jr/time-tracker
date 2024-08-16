6e6bb1082e28313f16ce70f8b509f870c2b48b1e
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-17 00:16:27 +0400

    [skip ci] fix time data types

--- src/main/java/sergi/example/task/dto/TaskDTO.java
+++ src/main/java/sergi/example/task/dto/TaskDTO.java
@@ -1,9 +1,10 @@
 package sergi.example.task.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -14,9 +15,11 @@ public class TaskDTO {
 
     private String description;
 
-    private LocalDate startedAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp startedAt;
 
-    private LocalDate finishedAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp finishedAt;
 
     private Long userId;
 }
