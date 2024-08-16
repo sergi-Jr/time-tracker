295043f6d828dac28f4a28b4381f86bca8ce1ed8
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-17 00:16:09 +0400

    [skip ci] add start-, end tracking logic

--- src/main/java/sergi/example/task/controller/TaskController.java
+++ src/main/java/sergi/example/task/controller/TaskController.java
@@ -1,16 +1,17 @@
 package sergi.example.task.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sergi.example.task.dal.TaskService;
 import sergi.example.task.dto.TaskDTO;
 import sergi.example.task.dto.TaskUpdateDTO;
+
+import java.sql.Timestamp;
 
 @RestController
 @RequestMapping("/tasks")
@@ -22,12 +23,16 @@ public class TaskController {
     @PutMapping(path = "/{id}/start-tracking", produces = MediaType.APPLICATION_JSON_VALUE)
     public TaskDTO startTracking(@PathVariable Long id) {
         TaskUpdateDTO data = new TaskUpdateDTO();
-        //data.setStartedAt();
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        data.setStartedAt(JsonNullable.of(currentTime));
         return taskService.update(id, data);
     }
 
     @PutMapping(path = "/{id}/end-tracking", produces = MediaType.APPLICATION_JSON_VALUE)
-    public TaskDTO endTracking(@PathVariable Long id, @RequestBody TaskUpdateDTO data) {
+    public TaskDTO endTracking(@PathVariable Long id) {
+        TaskUpdateDTO data = new TaskUpdateDTO();
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        data.setFinishedAt(JsonNullable.of(currentTime));
         return taskService.update(id, data);
     }
 }
