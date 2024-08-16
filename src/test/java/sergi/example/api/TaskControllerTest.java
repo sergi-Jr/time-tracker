0071f8ab0eb9a5c30217e9f1af389fb0db5a2d34
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-17 00:17:12 +0400

    [skip ci] add task controller tests

---
+++ src/test/java/sergi/example/api/TaskControllerTest.java
@@ -0,0 +1,64 @@
+package sergi.example.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import sergi.example.task.Task;
+import sergi.example.task.dal.TaskRepository;
+import utils.InitData;
+
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private InitData init;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    @Order(1)
+    void testStartTracking() throws Exception {
+        Task task = init.task();
+        taskRepository.save(task);
+
+        var request = put("/tasks/" + task.getId() + "/start-tracking");
+        mockMvc.perform(request).andExpect(status().isOk());
+
+        Optional<Task> opActual = taskRepository.findById(task.getId());
+        assertThat(opActual).isNotNull();
+
+        Task actual = opActual.get();
+        assertThat(actual.getStartedAt()).isNotNull();
+    }
+
+    @Test
+    @Order(2)
+    void testEndTracking() throws Exception {
+        Task task = init.task();
+        taskRepository.save(task);
+
+        var request = put("/tasks/" + task.getId() + "/end-tracking");
+        mockMvc.perform(request).andExpect(status().isOk());
+
+        Optional<Task> opActual = taskRepository.findById(task.getId());
+        assertThat(opActual).isNotNull();
+
+        Task actual = opActual.get();
+        assertThat(actual.getFinishedAt()).isNotNull();
+    }
+}
