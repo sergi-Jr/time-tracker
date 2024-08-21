b2e12c1a0d228e333ec067dc6d96b780fb574e72
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-21 11:29:51 +0400

    add date time formatter

--- src/main/java/sergi/example/config/JacksonConfig.java
+++ src/main/java/sergi/example/config/JacksonConfig.java
@@ -1,10 +1,13 @@
 package sergi.example.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfig {
@@ -13,6 +16,8 @@ public class JacksonConfig {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         builder.serializationInclusion(JsonInclude.Include.NON_NULL)
                 .modulesToInstall(new JsonNullableModule());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        builder.serializers(new LocalDateTimeSerializer(formatter));
         return builder;
     }
 }
