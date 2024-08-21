7935ce63c24638ecbcf4b041eb8c9edfca5d1525
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-21 11:35:55 +0400

    add method for sampling of time costs by task

--- src/main/java/sergi/example/task/dal/TaskRepository.java
+++ src/main/java/sergi/example/task/dal/TaskRepository.java
@@ -1,10 +1,20 @@
 package sergi.example.task.dal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sergi.example.task.Task;
+import sergi.example.task.dto.TaskStatsDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    @Query(name = "stats_native_query_dto", nativeQuery = true)
+    List<TaskStatsDTO> findTasksStatsByUserIdAndDateBetween(@Param("userId") long userId,
+                                                            @Param("start") LocalDateTime start,
+                                                            @Param("end") LocalDateTime end);
 }
