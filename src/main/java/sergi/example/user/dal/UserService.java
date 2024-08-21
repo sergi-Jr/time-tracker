d6956258623ec440ecbe35f82891f01e9bac8341
sergi-Jr <burashnikov.sergei1@mail.ru>
2024-08-21 11:31:46 +0400

    add method which returns user time spent for tasks

--- src/main/java/sergi/example/user/dal/UserService.java
+++ src/main/java/sergi/example/user/dal/UserService.java
@@ -1,24 +1,27 @@
 package sergi.example.user.dal;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sergi.example.exception.ResourceNotFoundException;
+import sergi.example.task.dal.TaskRepository;
+import sergi.example.task.dto.TaskStatsDTO;
 import sergi.example.user.User;
 import sergi.example.user.dto.UserCreateDTO;
 import sergi.example.user.dto.UserDTO;
 import sergi.example.user.dto.UserUpdateDTO;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-
+    private final TaskRepository taskRepository;
     private final UserMapper userMapper;
-
-    public UserService(UserRepository repository, UserMapper mapper) {
-        userRepository = repository;
-        userMapper = mapper;
-    }
 
     @Transactional
     public UserDTO create(UserCreateDTO data) {
@@ -34,5 +37,13 @@ public class UserService {
         userMapper.update(data, model);
         userRepository.save(model);
         return userMapper.map(model);
+    }
+
+    public List<TaskStatsDTO> getTrackingStats(String username, LocalDate start, LocalDate end) {
+        long userId = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found, email: " + username)).getId();
+
+        return taskRepository
+                .findTasksStatsByUserIdAndDateBetween(userId, start.atStartOfDay(), end.atTime(LocalTime.MAX));
     }
 }
